aa9eef62bfbcf7c73f256272b50e9962d9965923
mindctrl <dailyrants@gmail.com>
2012-02-04 11:59:14 -0500

    Add largebutton shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -16,3 +16,14 @@ function alienship_button($atts, $content = null) {
    }
 }
 add_shortcode('button', 'alienship_button');
+
+/* [largebutton] shortcode. Options for type= are "primary", "info", "success", "warning", "danger" */
+function alienship_largebutton($atts, $content = null) {
+   extract(shortcode_atts(array('link' => '#', 'type' => 'btn'), $atts));
+   if (isset($type)) {
+   return '<a class="btn btn-large btn-'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';   	
+   } else {
+   return '<a class="btn btn-large" href="'.$link.'">' . do_shortcode($content) . '</a>';
+   }
+}
+add_shortcode('largebutton', 'alienship_largebutton');

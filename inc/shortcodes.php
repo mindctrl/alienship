7fc7205bfe2e89615a8d245060cfdfdd8ec86a54
mindctrl <dailyrants@gmail.com>
2012-02-04 12:26:53 -0500

    Fix button shortcode mistake

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -6,13 +6,15 @@
  * @since Alien Ship 0.1
  */
 
+/* =Buttons
+----------------------------------------------- */
 /* [button] shortcode. Options for type= are "primary", "info", "success", "warning", "danger" */
 function alienship_button($atts, $content = null) {
    extract(shortcode_atts(array('link' => '#', 'type' => 'btn'), $atts));
-   if (isset($type)) {
+   if ($type != "btn") {
    return '<a class="btn btn-'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';   	
    } else {
-   return '<a class="btn" href="'.$link.'">' . do_shortcode($content) . '</a>';
+   return '<a class="'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';
    }
 }
 add_shortcode('button', 'alienship_button');
@@ -20,7 +22,7 @@ add_shortcode('button', 'alienship_button');
 /* [largebutton] shortcode. Options for type= are "primary", "info", "success", "warning", "danger" */
 function alienship_largebutton($atts, $content = null) {
    extract(shortcode_atts(array('link' => '#', 'type' => 'btn'), $atts));
-   if (isset($type)) {
+   if ($type != "btn") {
    return '<a class="btn btn-large btn-'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';   	
    } else {
    return '<a class="btn btn-large" href="'.$link.'">' . do_shortcode($content) . '</a>';

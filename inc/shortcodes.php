c5603efa63f8fedfb7e3cb03df2f2abffadc41fd
mindctrl <dailyrants@gmail.com>
2012-02-05 14:23:24 -0500

    Added panels feature and shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -6,49 +6,6 @@
  * @since Alien Ship 0.2
  */
 
-/* =Buttons
------------------------------------------------ */
-/* [button] shortcode. Options for type= are "primary", "info", "success", "warning", "danger" */
-function alienship_button($atts, $content = null) {
-   extract(shortcode_atts(array('link' => '#', 'type' => 'btn'), $atts));
-   if ($type != "btn") {
-   return '<a class="btn btn-'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';
-   } else {
-   return '<a class="'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';
-   }
-}
-add_shortcode('button', 'alienship_button');
-
-/* [largebutton] shortcode. Options for type= are "primary", "info", "success", "warning", "danger" */
-function alienship_largebutton($atts, $content = null) {
-   extract(shortcode_atts(array('link' => '#', 'type' => 'btn'), $atts));
-   if ($type != "btn") {
-   return '<a class="btn btn-large btn-'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';
-   } else {
-   return '<a class="'.$type.' '.$type.'-large" href="'.$link.'">' . do_shortcode($content) . '</a>';
-   }
-}
-add_shortcode('largebutton', 'alienship_largebutton');
-
-/* =Labels
------------------------------------------------ */
-function alienship_label($atts, $content = null) {
-   extract(shortcode_atts(array('type' => 'label'), $atts));
-   if ($type != "label") {
-   return '<span class="label label-'.$type.'">' . do_shortcode($content) . '</span>';
-   } else {
-   return '<span class="'.$type.'">' . do_shortcode($content) . '</span>';
-   }
-}
-add_shortcode('label', 'alienship_label');
-
-/* =Wells
------------------------------------------------ */
-function alienship_well($atts, $content = null) {
-   return '<div class="well">' . do_shortcode($content) .'</div>';
-}
-add_shortcode('well', 'alienship_well');
-
 /* =Alerts - Types are 'info', 'error', 'success', and unspecified(which displays a default color). Specify a heading text. See example.
  *  Example: [alert type="success" heading="Congrats!"]You won the lottery![/alert]
 ----------------------------------------------- */
@@ -61,3 +18,69 @@ function alienship_alert($atts, $content = null) {
    }
 }
 add_shortcode('alert', 'alienship_alert');
+
+
+
+/* =Buttons
+----------------------------------------------- */
+/* [button] shortcode. Options for type= are "primary", "info", "success", "warning", "danger"
+ *  Example: [button type="info" link="http://yourlink.com"]Button Text[/button] */
+function alienship_button($atts, $content = null) {
+   extract(shortcode_atts(array('link' => '#', 'type' => 'btn'), $atts));
+   if ($type != "btn") {
+   return '<a class="btn btn-'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';
+   } else {
+   return '<a class="'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';
+   }
+}
+add_shortcode('button', 'alienship_button');
+
+/* [largebutton] shortcode. Options for type= are "primary", "info", "success", "warning", "danger"
+ *  Example: [largebutton type="info" link="http://yourlink.com"]Large Button Text[/largebutton] */
+function alienship_largebutton($atts, $content = null) {
+   extract(shortcode_atts(array('link' => '#', 'type' => 'btn'), $atts));
+   if ($type != "btn") {
+   return '<a class="btn btn-large btn-'.$type.'" href="'.$link.'">' . do_shortcode($content) . '</a>';
+   } else {
+   return '<a class="'.$type.' '.$type.'-large" href="'.$link.'">' . do_shortcode($content) . '</a>';
+   }
+}
+add_shortcode('largebutton', 'alienship_largebutton');
+
+
+
+/* =Labels
+----------------------------------------------- 
+* [label] shortcode. Options for type= are "important", "info", "success", and "warning". If you don't specify a type there is a default style. 
+* Example: [label type="important"]Label text[/label] */
+function alienship_label($atts, $content = null) {
+   extract(shortcode_atts(array('type' => 'label'), $atts));
+   if ($type != "label") {
+   return '<span class="label label-'.$type.'">' . do_shortcode($content) . '</span>';
+   } else {
+   return '<span class="'.$type.'">' . do_shortcode($content) . '</span>';
+   }
+}
+add_shortcode('label', 'alienship_label');
+
+
+
+/* =Panels
+----------------------------------------------- 
+* [panel] shortcode.
+* Example: [panel]Your panel text here.[/panel] */
+function alienship_panel($atts, $content = null) {
+   return '<div class="panel">' . do_shortcode($content) .'</div>';
+}
+add_shortcode('panel', 'alienship_panel');
+
+
+
+/* =Wells
+-----------------------------------------------
+* [well] shortcode.
+* Example: [well]Your text here.[/well] */
+function alienship_well($atts, $content = null) {
+   return '<div class="well">' . do_shortcode($content) .'</div>';
+}
+add_shortcode('well', 'alienship_well');

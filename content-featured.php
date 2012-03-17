f5ac95992f7b2d8ddcb444a52b524a7bfbb5baff
mindctrl <dailyrants@gmail.com>
2012-03-17 10:39:14 -0400

    Bug fix

--- content-featured.php
+++ content-featured.php
@@ -5,7 +5,7 @@
  */
 ?>
     <div class="item">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo get_the_post_thumbnail( $post_id, array(of_get_option('alienship_featured_posts_image_width'), of_get_option('alienship_featured_posts_image_height')), array('title' => trim(strip_tags( $attachment->post_title )), )); ?></a>
+      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo get_the_post_thumbnail( ''. $post->ID .'', array(of_get_option('alienship_featured_posts_image_width'), of_get_option('alienship_featured_posts_image_height')), array('title' => "" )); ?></a>
       <div class="carousel-caption">
         <h4><?php the_title(); ?></h4>
       </div><!-- .carousel-caption -->

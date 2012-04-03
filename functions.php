49e0afc89c1b9d7a3491f7866a45e87fcfda25b6
mindctrl <dailyrants@gmail.com>
2012-04-02 22:31:29 -0400

    Added alienship_locate_template_uri function to improve child theming. Changed Bootstrap CSS loading to use this function.

--- functions.php
+++ functions.php
@@ -197,6 +197,37 @@ function alienship_options_scripts() { ?>
 add_action('optionsframework_custom_scripts', 'alienship_options_scripts');
 endif;
 
+/**
+ * Snatched from future release code in WordPress repo.
+ * 
+ * Retrieve the URI of the highest priority template file that exists.
+ *
+ * Searches in the stylesheet directory before the template directory so themes
+ * which inherit from a parent theme can just overload one file.
+ *
+ * @param string|array $template_names Template file(s) to search for, in order.
+ * @return string The URI of the file if one is located.
+ */
+if ( ! function_exists( 'alienship_locate_template_uri' ) ):
+function alienship_locate_template_uri( $template_names ) {
+ $located = '';
+ foreach ( (array) $template_names as $template_name ) {
+   if ( !$template_name )
+     continue;
+   if ( file_exists(get_stylesheet_directory() . '/' . $template_name)) {
+     $located = get_stylesheet_directory_uri() . '/' . $template_name;
+     break;
+   } else if ( file_exists(get_template_directory() . '/' . $template_name) ) {
+     $located = get_template_directory_uri() . '/' . $template_name;
+     break;
+   }
+ }
+
+ return $located;
+}
+endif;
+
+
 /* Stop WordPress from adding those annoying closing paragraph tags */
 // remove_filter( 'the_content', 'wpautop' );
 // remove_filter( 'the_excerpt', 'wpautop' );
--- inc/bootstrap-css.php
+++ inc/bootstrap-css.php
@@ -7,9 +7,9 @@
  */
 ?>
 <!-- Load Bootstrap CSS. Javascript is loaded in functions.php -->
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo alienship_locate_template_uri('css/bootstrap.min.css'); ?>">
 
 <?php if ( of_get_option('alienship_responsive',1) ) { ?>
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/bootstrap-responsive.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo alienship_locate_template_uri('css/bootstrap-responsive.min.css'); ?>">
 <?php } ?>
 <!-- End of Bootstrap CSS -->

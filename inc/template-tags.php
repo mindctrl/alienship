05f1b2303073cace3335268f0ca37230ab524945
mindctrl <dailyrants@gmail.com>
2012-04-20 15:47:15 -0400

    Add numbered page nav function.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -465,3 +465,60 @@ function alienship_archive_get_posts() {
   }
 }
 endif;
+
+/*
+function alienship_page_nav($before = '', $after = '') {
+  global $wpdb, $wp_query;
+  $request = $wp_query->request;
+  $posts_per_page = intval(get_query_var('posts_per_page'));
+  $paged = intval(get_query_var('paged'));
+  $numposts = $wp_query->found_posts;
+  $max_page = $wp_query->max_num_pages;
+  if ( $numposts <= $posts_per_page ) { return; }
+  if(empty($paged) || $paged == 0) {
+    $paged = 1;
+  }
+  $pages_to_show = 4;
+  $pages_to_show_minus_1 = $pages_to_show-1;
+  $half_page_start = floor($pages_to_show_minus_1/2);
+  $half_page_end = ceil($pages_to_show_minus_1/2);
+  $start_page = $paged - $half_page_start;
+  if($start_page <= 0) {
+    $start_page = 1;
+  }
+  $end_page = $paged + $half_page_end;
+  if(($end_page - $start_page) != $pages_to_show_minus_1) {
+    $end_page = $start_page + $pages_to_show_minus_1;
+  }
+  if($end_page > $max_page) {
+    $start_page = $max_page - $pages_to_show_minus_1;
+    $end_page = $max_page;
+  }
+  if($start_page <= 0) {
+    $start_page = 1;
+  }
+  echo $before.'<nav id="numbered-nav-below" class="pagination"><ul class="alienship_page_nav">'."";
+  if ($start_page >= 2 && $pages_to_show < $max_page) {
+    $first_page_text = "First";
+    echo '<li class="alienship-first-page-link"><a href="'.get_pagenum_link().'" title="'.$first_page_text.'">'.$first_page_text.'</a></li>';
+  }
+  echo '<li class="alienship-prev-link">';
+  previous_posts_link('&laquo;');
+  echo '</li>';
+  for($i = $start_page; $i  <= $end_page; $i++) {
+    if($i == $paged) {
+      echo '<li class="alienship-current-page-link active">'.$i.'</li>';
+    } else {
+      echo '<li><a href="'.get_pagenum_link($i).'">'.$i.'</a></li>';
+    }
+  }
+  echo '<li class="alienship-next-link">';
+  next_posts_link('&raquo;');
+  echo '</li>';
+  if ($end_page < $max_page) {
+    $last_page_text = "Last";
+    echo '<li class="alienship-last-page-link"><a href="'.get_pagenum_link($max_page).'" title="'.$last_page_text.'">'.$last_page_text.'</a></li>';
+  }
+  echo '</ul></nav>'.$after."";
+}
+*/

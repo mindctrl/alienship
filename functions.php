1bbfc03ad51d0fa4f579bff150642159c34b42be
mindctrl <dailyrants@gmail.com>
2012-04-24 23:13:53 -0400

    Ditch $locale.php

--- functions.php
+++ functions.php
@@ -75,12 +75,7 @@ function alienship_setup() {
    */
   load_theme_textdomain( 'alienship', get_template_directory() . '/languages' );
 
-  $locale = get_locale();
-  $locale_file = get_template_directory() . "/languages/$locale.php";
-  if ( is_readable( $locale_file ) )
-  	require_once( $locale_file );
 
-  
   /* Add default posts and comments RSS feed links to head */
   add_theme_support( 'automatic-feed-links' );
 

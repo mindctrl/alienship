7a81b647e3b6f47c66e5e990fd1171d95b76eb71
mindctrl <dailyrants@gmail.com>
2012-05-30 16:57:59 -0400

    X-UA-Compatible change

--- inc/parts/meta.php
+++ inc/parts/meta.php
@@ -7,9 +7,7 @@
  */
 ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<!-- Try overriding IE8/9 "Display all websites in Compatibility View" option -->
-<!--[if IE 8]> <meta http-equiv="X-UA-Compatible" content="IE=8" /> <![endif]-->
-<!--[if IE 9]> <meta http-equiv="X-UA-Compatible" content="IE=9" /> <![endif]-->
+<!-- [if IE]> <meta http-equiv="X-UA-Compatible" content="IE=Edge"/> <! [endif]-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />

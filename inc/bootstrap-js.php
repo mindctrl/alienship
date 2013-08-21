d41ca13e098681325ae7bdb58cbb1b84e5ba26e8
mindctrl <dailyrants@gmail.com>
2013-08-21 10:51:29 -0400

    Update version string in Bootstrap JS script loader.

--- inc/bootstrap-js.php
+++ inc/bootstrap-js.php
@@ -8,7 +8,7 @@
 function alienship_bootstrap_js_loader() {
 
 	// Bootstrap JS components - Drop a custom build in your child theme 'js' folder to override this one.
-	wp_enqueue_script( 'bootstrap.js', alienship_locate_template_uri( 'js/bootstrap.min.js' ), array( 'jquery' ), '3.0.0-wip', true );
+	wp_enqueue_script( 'bootstrap.js', alienship_locate_template_uri( 'js/bootstrap.min.js' ), array( 'jquery' ), '3.0.0', true );
 
 	// Bootstrap helper script
 	wp_enqueue_script('alienship-helper.js', alienship_locate_template_uri( 'js/alienship-helper.js' ), array('jquery'),'1.0', true);

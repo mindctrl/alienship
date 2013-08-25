bca619e082e1dcecb8ff362250455c16fe38bdf5
mindctrl <dailyrants@gmail.com>
2013-08-25 15:35:38 -0400

    Bring back excerpt functionality.

--- templates/parts/content-standard.php
+++ templates/parts/content-standard.php
@@ -12,13 +12,49 @@ do_action( 'alienship_post_before' ); ?>
 	do_action( 'alienship_entry_title' ); ?>
 
 	<div class="entry-content">
-		<?php if ( has_post_thumbnail() ) { ?>
-			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'alienship' ), the_title_attribute( 'echo=0' ) ); ?>"><?php echo get_the_post_thumbnail( $post->ID, 'thumbnail', array( 'class' => 'alignleft', 'title' => "" ) ); ?></a>
+		<?php
+		global $featured_header_image;
+
+		// If we have a featured image and it's not a fatty
+		if ( has_post_thumbnail() && $featured_header_image == 'no' ) {
+
+			$size = '';
+			$align = '';
+
+			// Do medium, aligned right on singular views
+			if ( is_singular() ) {
+
+				$size = 'medium';
+				$align = 'alignright';
+
+			// Do thumbnail, aligned left on archive views
+			} else {
+
+				$size = 'thumbnail';
+				$align = 'alignleft';
+
+			} ?>
+
+
+			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'alienship' ), the_title_attribute( 'echo=0' ) ); ?>">
+				<?php echo get_the_post_thumbnail( $post->ID, $size, array( 'class' => $align, 'title' => "" ) ); ?>
+			</a>
+
 		<?php }
-		the_content( __( 'Continue Reading &rarr;', 'alienship' ) );
+
+		if ( has_excerpt() && ! is_singular() ) {
+
+			the_excerpt(); ?>
+			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'alienship' ), the_title_attribute( 'echo=0' ) ); ?>"><?php _e( 'Continue Reading &raquo;', 'alienship' ); ?></a>
+
+		<?php } else {
+
+			the_content( __( 'Continue Reading &raquo;', 'alienship' ) );
+
+		}
 
 		wp_link_pages(); ?>
-	</div>
+	</div><!-- .entry-content -->
 
 	<footer class="entry-meta">
 		<?php
@@ -28,7 +64,7 @@ do_action( 'alienship_post_before' ); ?>
 		if (of_get_option('alienship_post_tags',1) && is_single() || of_get_option('alienship_post_tags_posts_page',1) && !is_single() ) { do_action( 'alienship_post_tags' ); } // show post tags?
 		if (of_get_option('alienship_post_comments_link',1) ) { do_action( 'alienship_post_comments_link' ); }
 		edit_post_link( __( ' Edit', 'alienship' ), '<span class="edit-link pull-right"><i class="glyphicon glyphicon-pencil"></i>', '</span>' ); // display the edit link ?>
-	</footer><!-- .entry-meta -->
+	</footer>
 
 	<?php do_action( 'alienship_post_inside_after' ); ?>
 </article><!-- #post-<?php the_ID(); ?> -->

6ec3f3f969955f61a606c20cb73507576d049d0a
mindctrl <dailyrants@gmail.com>
2013-09-21 10:44:58 -0400

    Make string translatable in navbar search box.

--- templates/parts/menu-top.php
+++ templates/parts/menu-top.php
@@ -26,7 +26,7 @@
 			if ( of_get_option('alienship_search_bar', '1') ) { ?>
 				<form id="searchform" class="navbar-form navbar-right" role="search" action="<?php echo site_url(); ?>" method="get">
 					<div class="form-group">
-						<input id="s" name="s" type="text" class="form-control" placeholder="Search" value="<?php echo esc_attr( get_search_query() ); ?>">
+						<input id="s" name="s" type="text" class="form-control" placeholder="<?php esc_attr_e( 'Search &hellip;', 'alienship' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>">
 					</div>
 					<button id="searchsubmit" type="submit" name="submit" class="btn btn-default">Submit</button>
 				</form>

6cb8884a4b894472d7d29ea12ec84f6e3138b91d
mindctrl <dailyrants@gmail.com>
2013-10-02 20:44:31 -0400

    Update jQuery type selector.

--- js/alienship-helper.js
+++ js/alienship-helper.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
 	// Style form controls
 	jQuery( "input[name='s']" ).addClass( 'form-control' );
-	jQuery( 'select.postform' ).addClass( 'form-control input-sm' );
+	jQuery( type="select" ).addClass( 'form-control input-sm' );
 	jQuery( 'input#submit' ).addClass( 'btn btn-default' );
 });

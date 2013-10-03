5b633e7b24a4c0af150aa203951a33fbd37e42dd
mindctrl <dailyrants@gmail.com>
2013-10-03 17:54:51 -0400

    Style search input in markup, not JS.

--- js/alienship-helper.js
+++ js/alienship-helper.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function(){
 	// Style form controls
-	jQuery( "input[name='s']" ).addClass( 'form-control' );
 	jQuery( type="select" ).addClass( 'form-control input-sm' );
 	jQuery( 'input#submit' ).addClass( 'btn btn-default' );
 	// Tables
--- searchform.php
+++ searchform.php
@@ -9,7 +9,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php _ex( 'Search:', 'label', 'alienship' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'alienship' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+		<input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'alienship' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'alienship' ); ?>">
+	<input type="submit" class="search-submit btn btn-default" value="<?php echo esc_attr_x( 'Search', 'submit button', 'alienship' ); ?>">
 </form>

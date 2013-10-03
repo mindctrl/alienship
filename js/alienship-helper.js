4144295386e16a05e8533d62cfdc17b51161362b
mindctrl <dailyrants@gmail.com>
2013-10-02 22:00:25 -0400

    Use jQuery to add table class to tables to ensure proper styling.

--- js/alienship-helper.js
+++ js/alienship-helper.js
@@ -3,4 +3,6 @@ jQuery(document).ready(function(){
 	jQuery( "input[name='s']" ).addClass( 'form-control' );
 	jQuery( type="select" ).addClass( 'form-control input-sm' );
 	jQuery( 'input#submit' ).addClass( 'btn btn-default' );
+	// Tables
+	jQuery( 'table' ).addClass( 'table' );
 });

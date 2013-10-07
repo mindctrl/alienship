fde584c60b72cade7c9ead58abb6ebe242db7a41
mindctrl <dailyrants@gmail.com>
2013-10-07 11:56:25 -0400

    First round of revamping template tags and hooks.

--- templates/parts/content-image.php
+++ templates/parts/content-image.php
@@ -7,9 +7,10 @@
 do_action( 'alienship_post_before' ); ?>
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
-	do_action( 'alienship_post_inside_before' );
-
-	do_action( 'alienship_entry_title' ); ?>
+	do_action( 'alienship_post_top' );
+	do_action( 'alienship_entry_header' );
+	do_action( 'alienship_entry_content_before' );
+	?>
 
 	<div class="entry-content">
 
@@ -27,18 +28,10 @@ do_action( 'alienship_post_before' ); ?>
 
 		wp_link_pages(); ?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-meta">
-		<?php
-		do_action( 'alienship_posted_on' ); // show published date
-		do_action( 'alienship_post_author' ); // show post author
-		do_action( 'alienship_post_categories' ); // show post categories
-		do_action( 'alienship_post_tags' ); // show post tags
-		do_action( 'alienship_post_comments_link' ); // show comment link
-		edit_post_link( __( ' Edit', 'alienship' ), '<span class="edit-link pull-right"><i class="glyphicon glyphicon-pencil"></i>', '</span>' ); // display the edit link
-		?>
-	</footer>
-
-	<?php do_action( 'alienship_post_inside_after' ); ?>
+	<?php
+	do_action( 'alienship_entry_content_after' );
+	do_action( 'alienship_entry_footer' );
+	do_action( 'alienship_post_bottom' );
+	?>
 </article><!-- #post-<?php the_ID(); ?> -->
 <?php do_action( 'alienship_post_after' ); ?>
--- templates/parts/content.php
+++ templates/parts/content.php
@@ -7,10 +7,10 @@
 do_action( 'alienship_post_before' ); ?>
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
-	do_action( 'alienship_post_inside_before' );
-
-	do_action( 'alienship_entry_title' ); ?>
-
+	do_action( 'alienship_post_top' );
+	do_action( 'alienship_entry_header' );
+	do_action( 'alienship_entry_content_before' );
+	?>
 	<div class="entry-content">
 		<?php
 
@@ -31,9 +31,8 @@ do_action( 'alienship_post_before' ); ?>
 
 		// On singular views, display post thumbnails in the post body if it's not big enough to be a header image
 		else {
-
-			global $featured_header_image;
-			if ( has_post_thumbnail() && 'no' == $featured_header_image ) { ?>
+			$header_image = alienship_get_header_image( get_the_ID() );
+			if ( has_post_thumbnail() && 'no' == $header_image['featured'] ) { ?>
 
 				<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'alienship' ), the_title_attribute( 'echo=0' ) ); ?>">
 					<?php echo get_the_post_thumbnail( $post->ID, 'medium', array( 'class' => 'alignright', 'title' => "" ) ); ?>
@@ -46,18 +45,10 @@ do_action( 'alienship_post_before' ); ?>
 
 		wp_link_pages(); ?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-meta">
-		<?php
-		do_action( 'alienship_posted_on' ); // show published date
-		do_action( 'alienship_post_author' ); // show post author
-		do_action( 'alienship_post_categories' ); // show post categories
-		do_action( 'alienship_post_tags' ); // show post tags
-		do_action( 'alienship_post_comments_link' ); // show comment link
-		edit_post_link( __( ' Edit', 'alienship' ), '<span class="edit-link pull-right"><i class="glyphicon glyphicon-pencil"></i>', '</span>' ); // display the edit link
-		?>
-	</footer>
-
-	<?php do_action( 'alienship_post_inside_after' ); ?>
+	<?php
+	do_action( 'alienship_entry_content_after' );
+	do_action( 'alienship_entry_footer' );
+	do_action( 'alienship_post_bottom' );
+	?>
 </article><!-- #post-<?php the_ID(); ?> -->
 <?php do_action( 'alienship_post_after' ); ?>

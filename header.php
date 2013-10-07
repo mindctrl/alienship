873393ed47e5067760d006b4742a360a6c06226f
mindctrl <dailyrants@gmail.com>
2013-10-07 12:29:13 -0400

    Revamp header image code.

--- header.php
+++ header.php
@@ -14,8 +14,6 @@
 		<?php get_template_part( '/templates/parts/meta' ); ?>
 		<title><?php wp_title( '&#8226;', true, 'right' ); ?></title>
 
-		<?php if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js" type="text/javascript"></script>
 		<![endif]-->
@@ -26,12 +24,11 @@
 	</head>
 
 <body <?php body_class(); ?>>
-	<!--[if lt IE 8]><p class="browsehappy alert alert-danger">You are using an outdated browser. Please <a class="alert-link" href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
+	<!--[if lt IE 9]><p class="browsehappy alert alert-danger">You are using an outdated browser. Please <a class="alert-link" href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
 
 	<?php
-	if ( of_get_option('alienship_show_top_navbar',1) ) {
+	if ( of_get_option( 'alienship_show_top_navbar', 1 ) )
 		get_template_part( '/templates/parts/menu', 'top' );
-	}
 
 	// If not using Hero template, do header image and menu stuff
 	if ( ! is_page_template( 'templates/page-hero.php' ) ) { ?>
@@ -41,43 +38,18 @@
 			<?php do_action( 'alienship_header_before' ); ?>
 			<header id="masthead" role="banner">
 				<?php
-				do_action( 'alienship_header_inside' );
-				alienship_header_title_and_description();
 
-				// Check for header image
-				$header_image = get_header_image();
-				global $featured_header_image;
+				/**
+				 * Display site title and description.
+				 * Hooked in /inc/template-tags.php
+				 */
+				do_action( 'alienship_site_title' );
+				do_action( 'alienship_site_description' );
 
-				// We have a header image
-				if ( $header_image ) {
+				// Header image
+				do_action( 'alienship_header_image' );
 
-					$header_image_width = get_theme_support( 'custom-header', 'width' );
-					$header_image_height = get_theme_support( 'custom-header', 'height' ); ?>
-
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<?php
-						// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-						if ( is_singular() && has_post_thumbnail( $post->ID ) /* $src, $width, $height */ && ( $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_height ) ) ) && $image[1] >= $header_image_width ) {
-
-							// We have a LARGE image
-							$featured_header_image = 'yes';
-							echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-
-						} else {
-
-							$featured_header_image = 'no';
-							$header_image_width  = get_custom_header()->width;
-							$header_image_height = get_custom_header()->height; ?>
-							<img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" class="header-image" alt="" />
-						<?php } ?>
-					</a>
-
-				<?php // end check for header image
-				} else {
-					// We didn't have a header image enabled at all
-					$featured_header_image = 'no';
-				}
-
+				// Main menu
 				if ( has_nav_menu('main') ) {
 					get_template_part( '/templates/parts/menu', 'main' );
 				} ?>

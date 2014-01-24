7d8afc361cc9a16db5bd0f43dcb7cd91004618fc
mindctrl <dailyrants@gmail.com>
2014-01-24 14:57:52 -0500

    Add display filter to site name in the title.

--- functions.php
+++ functions.php
@@ -242,7 +242,7 @@ function alienship_wp_title( $title, $sep ) {
 		return $title;
 
 	// Add the site name.
-	$title .= get_bloginfo( 'name' );
+	$title .= get_bloginfo( 'name', 'display' );
 
 	// Add the site description for the home/front page.
 	$site_description = get_bloginfo( 'description', 'display' );

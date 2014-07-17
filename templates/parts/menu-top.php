65a038f779d2248def2afe4c68a3a1628b1843f6
John Parris <public@johnparris.com>
2014-07-17 11:57:55 -0400

    Remove search box from top menu bar

--- templates/parts/menu-top.php
+++ templates/parts/menu-top.php
@@ -27,17 +27,7 @@
 				'walker'         => new wp_bootstrap_navwalker(),
 				'fallback_cb'    => 'wp_bootstrap_navwalker::fallback'
 				)
-			);
-
-			if ( of_get_option( 'alienship_search_bar', '1' ) ) { ?>
-				<form id="searchform" class="navbar-form navbar-right" role="search" action="<?php echo esc_url( site_url() ); ?>" method="get">
-					<div class="form-group">
-						<input id="s" name="s" type="text" class="form-control" placeholder="<?php esc_attr_e( 'Search &hellip;', 'alienship' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>">
-					</div>
-					<button id="searchsubmit" type="submit" name="submit" class="btn btn-default">Submit</button>
-				</form>
-			<?php } ?>
-
+			); ?>
 		</div>
 	</nav>
 <!-- End Top Menu -->

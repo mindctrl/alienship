f5f085861bebcb7f5bb5a502f28b7f810d36abb9
John Parris <public@johnparris.com>
2014-07-21 14:29:49 -0400

    No padding on paging navigation. Move featured image above title in prep for additional changes.

--- templates/parts/content-image.php
+++ templates/parts/content-image.php
@@ -6,19 +6,18 @@
 ?>
 
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php do_action( 'alienship_entry_header' ); ?>
-
-	<div class="entry-content">
-
-	<?php
-	// If we have a featured image
+	<?php 	// If we have a featured image
 	if ( has_post_thumbnail() ) { ?>
 		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'alienship' ), the_title_attribute( 'echo=0' ) ); ?>">
 			<?php echo get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'aligncenter', 'title' => "" ) ); ?>
 		</a>
 	<?php }
 
-		the_content();
+	do_action( 'alienship_entry_header' ); ?>
+
+	<div class="entry-content">
+
+		<?php the_content();
 
 		wp_link_pages(); ?>
 	</div><!-- .entry-content -->
--- templates/parts/content.php
+++ templates/parts/content.php
@@ -6,17 +6,17 @@
 ?>
 
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php do_action( 'alienship_entry_header' ); ?>
+	<?php if ( has_post_thumbnail() ) { ?>
+		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'alienship' ), the_title_attribute( 'echo=0' ) ); ?>">
+			<?php echo get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'aligncenter', 'title' => "" ) ); ?>
+		</a>
+		<?php
+	} // has_post_thumbnail()
+
+	do_action( 'alienship_entry_header' ); ?>
 
 	<div class="entry-content">
 		<?php
-
-		if ( has_post_thumbnail() ) { ?>
-			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'alienship' ), the_title_attribute( 'echo=0' ) ); ?>">
-				<?php echo get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'aligncenter', 'title' => "" ) ); ?>
-			</a>
-			<?php
-		} // has_post_thumbnail()
 
 		// Display excerpt on archives, full content in singular views.
 		is_singular() ? the_content() : the_excerpt();

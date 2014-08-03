c23b96b61bd9dd5780649f31ed0f0c95a815a130
John Parris <public@johnparris.com>
2014-08-02 21:41:53 -0400

    Remove alienship_categorized_blog function and related transient stuff.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -220,54 +220,6 @@ endif; // ends check for alienship_comment()
 
 
 
-/**
- * Returns true if a blog has more than 1 category
- *
- * @since 0.1
- */
-function alienship_categorized_blog() {
-
-	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
-
-		// Create an array of all the categories that are attached to posts
-		$all_the_cool_cats = get_categories( array(
-			'hide_empty' => 1,
-		) );
-
-		// Count the number of categories that are attached to the posts
-		$all_the_cool_cats = count( $all_the_cool_cats );
-
-		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
-	}
-
-	if ( '1' != $all_the_cool_cats ) {
-
-		// This blog has more than 1 category so alienship_categorized_blog should return true
-		return true;
-
-	} else {
-
-		// This blog has only 1 category so alienship_categorized_blog should return false
-		return false;
-	}
-}
-
-
-
-/**
- * Flush out the transients used in alienship_categorized_blog
- *
- * @since 0.1
- */
-function alienship_category_transient_flusher() {
-
-	delete_transient( 'all_the_cool_cats' );
-}
-add_action( 'edit_category', 'alienship_category_transient_flusher' );
-add_action( 'save_post', 'alienship_category_transient_flusher' );
-
-
-
 if ( ! function_exists( 'alienship_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.

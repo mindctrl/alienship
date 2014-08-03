64e0ca0ab827336d1581af5d559564a132df8482
John Parris <public@johnparris.com>
2014-08-02 21:38:27 -0400

    Remove old entry header functions

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -114,56 +114,6 @@ function alienship_do_sidebar( $prefix = false ) {
 
 
 
-/**
- * Print the opening markup for the entry header.
- *
- * @since 1.2.0
- *
- */
-function alienship_entry_header_markup_open() {
-	echo '<header class="entry-header">';
-}
-add_action( 'alienship_entry_header', 'alienship_entry_header_markup_open', 5 );
-
-
-
-/**
- * Set title to H1 if in single view, otherwise set it to H2
- * @since .75
- */
-function alienship_do_entry_title() {
-
-	$title = get_the_title();
-
-	if ( 0 === mb_strlen( $title ) )
-		return;
-
-	if ( is_singular() ) {
-		$entry_title = sprintf( '<h1 class="entry-title">%s</h1>', $title );
-
-	} else {
-		$entry_title = sprintf( '<h2 class="entry-title"><a class="entry-title" title="%s" rel="bookmark" href="%s">%s</a></h2>', the_title_attribute( 'echo=0' ), get_permalink(), $title );
-
-	}
-	echo apply_filters( 'alienship_entry_title_text', $entry_title );
-}
-add_action( 'alienship_entry_header', 'alienship_do_entry_title' );
-
-
-
-/**
- * Print the closing markup for the entry header.
- *
- * @since 1.2.0
- */
-function alienship_entry_header_markup_close() {
-	echo '</header>';
-}
-add_action( 'alienship_entry_header', 'alienship_entry_header_markup_close', 15 );
-
-
-
-
 if ( ! function_exists( 'alienship_content_nav' ) ):
 /**
  * Display navigation to next/previous pages when applicable

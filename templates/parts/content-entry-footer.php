a9bba636cf82ef6d10a5e7abbe58304d3e3ebb8c
John Parris <public@johnparris.com>
2014-08-02 21:14:42 -0400

    Rework the post entry footer

--- templates/parts/content-entry-footer.php
+++ templates/parts/content-entry-footer.php
@@ -5,40 +5,34 @@
  */
 ?>
 <footer class="entry-footer">
-	<?php
-	if( 'post' == get_post_type() ) :
+	<?php if( 'post' == get_post_type() ) :
 
 		/* translators: used between list items, there is a space after the comma */
 		$category_list = get_the_category_list( __( ', ', 'alienship' ) );
 
 		/* translators: used between list items, there is a space after the comma */
-		$tag_list = get_the_tag_list( '', __( ', ', 'alienship' ) );
+		$tag_list = get_the_tag_list( '', __( ', ', 'alienship' ) ); ?>
 
-		if ( '' != $tag_list ) {
-			$meta_text = '<span class="cat-links"><i class="glyphicon glyphicon-folder-open" title="'. __( 'Categories', 'alienship' ) .'"></i> ';
-			$meta_text .= __( '%1$s' );
-			$meta_text .= ' <span class="tags-links"><i class="glyphicon glyphicon-tags" title="'. __( 'Tags', 'alienship' ) .'"></i> ';
-			$meta_text .= __( '%2$s' );
+		<span class="cat-links">
+			<i class="glyphicon glyphicon-folder-open" title="<?php __( 'Categories', 'alienship' ); ?>"></i>
+			<?php echo $category_list; ?>
+		</span>
 
-		} else {
-			$meta_text = '<span class="cat-links"><i class="glyphicon glyphicon-folder-open" title="'. __( 'Categories', 'alienship' ) .'"></i> ';
-			$meta_text .= __( '%1$s' );
-		}
+		<?php if ( '' != $tag_list ) : ?>
+			<span class="tags-links">
+				<i class="glyphicon glyphicon-tags" title="<?php __( 'Tags', 'alienship' ); ?>"></i>
+				<?php echo $tag_list; ?>
+			</span>
+		<?php endif; ?>
 
-		printf(
-			$meta_text,
-			$category_list,
-			$tag_list
-		);
-
-		if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) { ?>
+		<?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
 			<span class="comments-link">
 				<i class="glyphicon glyphicon-comment"></i>
 				<?php comments_popup_link( __( ' Leave a comment', 'alienship' ), __( ' 1 Comment', 'alienship' ), __( ' % Comments', 'alienship' ) ); ?>
 			</span>
-		<?php }
+		<?php endif; ?>
 
-	endif; ?>
+	<?php endif; ?>
 
 	<?php edit_post_link( __( 'Edit', 'alienship' ), '<span class="edit-link pull-right">', '</span>' ); ?>
 </footer><!-- .entry-footer -->

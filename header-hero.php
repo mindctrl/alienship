863b6dd2d7c450545ceeed042e121a3175deeff4
John Parris <public@johnparris.com>
2014-08-10 15:36:23 -0400

    Update HTML5 Shiv. Fix paths.

--- header-hero.php
+++ header-hero.php
@@ -13,7 +13,7 @@
 <head>
 <?php get_template_part( '/templates/parts/meta' ); ?>
 <title><?php wp_title( '&#8226;', true, 'right' ); ?></title>
-<!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/assets/javascripts/html5shiv.js" type="text/javascript"></script><![endif]-->
+<!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5shiv.min.js" type="text/javascript"></script><![endif]-->
 
 <?php
 wp_head();

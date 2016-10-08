83f2072ef44bad45b530160c03c6cb7b213eaf6e
John Parris <public@johnparris.com>
2016-10-08 10:52:48 -0400

    Simplify image.php template

--- image.php
+++ image.php
@@ -29,7 +29,7 @@ get_header(); ?>
 								esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
 								get_the_title( $post->post_parent )
 							);
-							edit_post_link( __( 'Edit', 'alienship' ), '<span class="sep">|</span> <span class="edit-link">', '</span>' ); ?>
+							?>
 						</div><!-- .entry-meta -->
 					</header>
 
@@ -53,32 +53,17 @@ get_header(); ?>
 						?>
 					</div><!-- .entry-content -->
 
-						<nav role="navigation" id="image-navigation" class="image-navigation">
-							<ul class="pager">
-								<li><?php previous_image_link( false, __( '&laquo; Previous', 'alienship' ) ); ?></li>
-								<li><?php next_image_link( false, __( 'Next &raquo;', 'alienship' ) ); ?></li>
-							</ul>
-						</nav>
+					<nav role="navigation" id="image-navigation" class="image-navigation">
+						<ul class="pager">
+							<li><?php previous_image_link( false, __( '&laquo; Previous', 'alienship' ) ); ?></li>
+							<li><?php next_image_link( false, __( 'Next &raquo;', 'alienship' ) ); ?></li>
+						</ul>
+					</nav>
 
 					<footer class="entry-meta">
-						<?php
-						if ( comments_open() && pings_open() ) : // Comments and trackbacks open
-							printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'alienship' ), get_trackback_url() );
-
-						elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open
-							printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'alienship' ), get_trackback_url() );
-
-						elseif ( comments_open() && ! pings_open() ) : // Only comments open
-							_e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'alienship' );
-
-						elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed
-							_e( 'Both comments and trackbacks are currently closed.', 'alienship' );
-
-						endif;
-
-						edit_post_link( __( 'Edit', 'alienship' ), ' <span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-meta -->
-				</article><!-- #post-<?php the_ID(); ?> -->
+						<?php edit_post_link( __( 'Edit', 'alienship' ), ' <span class="edit-link">', '</span>' ); ?>
+					</footer>
+				</article>
 
 				<?php comments_template(); ?>
 
